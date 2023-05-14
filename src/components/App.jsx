9e07c869628faaeba34dd44e6ace12084c8d111b
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2023-05-14 20:25:32 +0200

    copied project with tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-import ContactsList from './Contacts/Contacts';
-import { FindNumberByName } from './Find/FindNumberByName';
-import { Form } from './Form/Form';
+// import ContactsList from './Contacts/Contacts';
+// import { FindNumberByName } from './Find/FindNumberByName';
+// import { Form } from './Form/Form';
+
+// export const App = () => {
+  
+//   return (
+//     <div>
+//       <h1>Phonebook</h1>
+//       <Form />
+//       <h2>Contacts</h2>
+//       <FindNumberByName />
+//       <ContactsList />
+//     </div>
+//   );
+// };
+
+import { useEffect, lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+import { refreshUser } from 'redux/auth/operations';
+import { useAuth } from 'hooks';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const RegisterPage = lazy(() => import('../pages/Register'));
+const LoginPage = lazy(() => import('../pages/Login'));
+const TasksPage = lazy(() => import('../pages/Tasks'));
 
 export const App = () => {
-  
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <Form />
-      <h2>Contacts</h2>
-      <FindNumberByName />
-      <ContactsList />
-    </div>
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<RegisterPage />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/tasks"
+          element={
+            <PrivateRoute redirectTo="/login" component={<TasksPage />} />
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,53 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsApi, filterReducer } from './contactsSlice';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactsApi, filterReducer } from './contactsSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     [contactsApi.reducerPath]: contactsApi.reducer,
+//     filter: filterReducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(contactsApi.middleware),
+// });
+
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { tasksReducer } from './tasks/slice';
+import { authReducer } from './auth/slice';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+// Persisting token field from auth slice to localstorage
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
-    [contactsApi.reducerPath]: contactsApi.reducer,
-    filter: filterReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
+    tasks: tasksReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(contactsApi.middleware),
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
+
