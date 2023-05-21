7b3731a342658cbc82d04ca2d2218782e593ffae
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2023-05-21 10:21:13 +0200

    implemented chakra-ui

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter basename="/goit-react-hw-08-phonebook">
-        <App />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
